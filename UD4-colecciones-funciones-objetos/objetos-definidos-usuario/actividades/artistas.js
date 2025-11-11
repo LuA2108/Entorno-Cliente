1e2e04ea470b4d68d2ed22265e27d9d902a249d0
Lucero <luceroan2108@gmail.com>
2025-11-11 20:59:41 +0100

    menu

---
+++ UD4-colecciones-funciones-objetos/objetos-definidos-usuario/actividades/artistas.js
@@ -0,0 +1,54 @@
+let opcion = -1;
+let menu = "---- Menu opciones ----<br>1. Artistas solistas.<br>\
+            2. Artista por edad.<br>\
+            3. Artistas con instrumentos.<br>\
+            4. Artistas por genero.<br>\
+            5. Artistas con mas discos.<br>\
+            6. Artistas con mas entradas vendidas.<br>\
+            7. Artista con mayor recaudacion.<br>\
+            8. Artistas segun el año del disco.<br>\
+            9. Artista con mas copias.<br>\
+            10. Salir.<br>\
+            Seleccione una opcion: ";
+
+
+do {
+    let opcion = parseInt(prompt(menu));
+    
+    switch (opcion) {
+        case 1:
+            
+            break;
+        
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            
+            break;
+        case 5:
+            
+            break;
+        case 6:
+            
+            break;
+        case 7:
+            
+            break;
+        case 8:
+            
+            break;
+        case 9:
+            
+            break;
+        case 10:
+            
+            break;
+        default:
+            break;
+    }
+
+} while (opcion != 10);
