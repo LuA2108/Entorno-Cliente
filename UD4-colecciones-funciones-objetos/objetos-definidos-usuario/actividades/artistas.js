02b2fd0541b10480345f94a1aa3f077a3acf9e47
Lucero <luceroan2108@gmail.com>
2025-11-13 03:39:16 +0100

    Funciones de javascript, uso de objetos

--- UD4-colecciones-funciones-objetos/objetos-definidos-usuario/actividades/artistas.js
+++ UD4-colecciones-funciones-objetos/objetos-definidos-usuario/actividades/artistas.js
@@ -1,53 +1,59 @@
-let opcion = -1;
-let menu = "---- Menu opciones ----<br>1. Artistas solistas.<br>\
-            2. Artista por edad.<br>\
-            3. Artistas con instrumentos.<br>\
-            4. Artistas por genero.<br>\
-            5. Artistas con mas discos.<br>\
-            6. Artistas con mas entradas vendidas.<br>\
-            7. Artista con mayor recaudacion.<br>\
-            8. Artistas segun el año del disco.<br>\
-            9. Artista con mas copias.<br>\
-            10. Salir.<br>\
-            Seleccione una opcion: ";
+function menu() {
+    const menu = "---- Menú de opciones ----\n" +
+        "1. Artistas solistas\n" +
+        "2. Artista por edad\n" +
+        "3. Artistas por instrumento\n" +
+        "4. Artistas por género\n" +
+        "5. Artistas con más discos\n" +
+        "6. Artistas con más entradas vendidas\n" +
+        "7. Artista con mayor recaudación\n" +
+        "8. Artistas según año del disco\n" +
+        "9. Artista con más copias\n" +
+        "10. Salir\n"+
+        "Elige una opcion: ";
+    
+    return prompt(menu);
+}
 
+let opcion = -1;
 
 do {
-    let opcion = parseInt(prompt(menu));
-    
+    opcion = Number(menu());
+
     switch (opcion) {
         case 1:
-            
+
             break;
-        
+
         case 2:
-            
+
             break;
         case 3:
-            
+
             break;
         case 4:
-            
+
             break;
         case 5:
-            
+
             break;
         case 6:
-            
+
             break;
         case 7:
-            
+
             break;
         case 8:
-            
+
             break;
         case 9:
-            
+
             break;
         case 10:
-            
+        console.log("Fin.");
             break;
         default:
+            console.log("");
             break;
     }
 
