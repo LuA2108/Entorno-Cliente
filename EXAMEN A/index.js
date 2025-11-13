868c04c1db81efd51615fa9fe94a7c2591705300
LuA2108 <luceroan@gmail.com>
2025-11-13 11:59:44 +0100

    g

---
+++ EXAMEN A/index.js
@@ -0,0 +1,45 @@
+
+let menu = true;
+
+while (menu) {
+    var opcion = prompt(
+    "Selecciona una opción:\n1. Ventas por Vendedor (componentes) \n2. Ventas por Sucursal (número) \n3. Cantidad Ventas Componente\n4. Presupuesto PC \n5. Ventas Totales por Vendedor (número) \n6. Salir");
+    switch (opcion) {
+        case "1":
+            let vendedor = prompt("Ingrese el nombre de un vendedor: ");
+
+            console.log(ventasVendedore(vendedor, ventas));
+
+        break;
+        
+        case "2":
+            let sucursal = prompt("Ingrese el nombre de una sucursal: ");
+
+            console.log("Ventas de sucursal"+ sucursal + " : "+ ventasSucursal(ventas, sucursal));
+        break;
+
+        case "3":
+            let componente = prompt("Ingrese el nombre de un componente de PC: ");
+        break;
+
+        case "4":
+            let componentes = prompt("Ingrese una lista de componente de PC (separadas `,`)");
+            let listaComponentes = componentes.slice();
+
+
+        break;
+        
+        case "5":
+            let vendedorA = prompt("Ingrese el nombre del vendedor: ");
+
+            console.log(ventasTotalesVendedore(ventas, vendedorA));
+        break;
+        
+        case "6":
+            alert("Adiós!");
+            menu = false;
+        break;
+
+    }
+}
+
---
+++ EXAMEN A/ventas.js
@@ -0,0 +1,38 @@
+const ventas = {
+    vendedores: ["Scooby-Doo", "Shaggy Rogers", "Vilma Dinkley", "Daphne Blake"],
+    sucursales: ['Torrevieja', 'Orihuela Costa'],
+    detalle: [
+      { fecha: new Date(2024, 1, 4), nombreVendedore: "Shaggy Rogers", componentesPC: ["Monitor GPRS 3000", "Motherboard ASUS 1500"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 0, 1), nombreVendedore: "Scooby-Doo", componentesPC: ["Monitor GPRS 3000", "Motherboard ASUS 1500"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 0, 2), nombreVendedore: "Shaggy Rogers", componentesPC: ["Monitor ASC 543", "Motherboard MZI"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 0, 10), nombreVendedore: "Scooby-Doo", componentesPC: ["Monitor ASC 543", "Motherboard ASUS 1200"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 0, 12), nombreVendedore: "Shaggy Rogers", componentesPC: ["Monitor GPRS 3000", "Motherboard ASUS 1200"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 12), nombreVendedore: "Vilma Dinkley", componentesPC: ["Monitor GPRS 3000", "HDD Toyiva"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 24), nombreVendedore: "Daphne Blake", componentesPC: ["Motherboard ASUS 1500", "HDD Wezter Dishital"], sucursal: "Orihuela Costa" },
+      { fecha: new Date(2024, 1, 1), nombreVendedore: "Scooby-Doo", componentesPC: ["Motherboard ASUS 1200", "RAM Quinston Fury"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 11), nombreVendedore: "Shaggy Rogers", componentesPC: ["Monitor ASC 543", "RAM Quinston"], sucursal: "Orihuela Costa" },
+      { fecha: new Date(2024, 1, 15), nombreVendedore: "Scooby-Doo", componentesPC: ["Motherboard ASUS 1200", "RAM Quinston Fury"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 12), nombreVendedore: "Vilma Dinkley", componentesPC: ["Motherboard ASUS 1500", "HDD Toyiva"], sucursal: "Orihuela Costa" },
+      { fecha: new Date(2024, 1, 21), nombreVendedore: "Shaggy Rogers", componentesPC: ["Motherboard ASUS 1200", "RAM Quinston"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 8), nombreVendedore: "Daphne Blake", componentesPC: ["Monitor ASC 543", "HDD Wezter Dishital"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 16), nombreVendedore: "Daphne Blake", componentesPC: ["Monitor GPRS 3000", "RAM Quinston Fury"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 27), nombreVendedore: "Vilma Dinkley", componentesPC: ["Motherboard ASUS 1200", "HDD Toyiva"], sucursal: "Orihuela Costa" },
+      { fecha: new Date(2024, 1, 22), nombreVendedore: "Shaggy Rogers", componentesPC: ["Monitor ASC 543", "HDD Wezter Dishital"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 5), nombreVendedore: "Scooby-Doo", componentesPC: ["Motherboard ASUS 1500", "RAM Quinston"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 1), nombreVendedore: "Shaggy Rogers", componentesPC: ["Motherboard ASUS 1200", "HDD Wezter Dishital"], sucursal: "Torrevieja" },
+      { fecha: new Date(2024, 1, 7), nombreVendedore: "Daphne Blake", componentesPC: ["Monitor GPRS 3000", "RAM Quinston"], sucursal: "Orihuela Costa" },
+      { fecha: new Date(2024, 1, 14), nombreVendedore: "Scooby-Doo", componentesPC: ["Motherboard ASUS 1200", "HDD Toyiva"], sucursal: "Torrevieja" }
+    ],
+    precios: [
+      { componente: "Monitor GPRS 3000", precio: 200 },
+      { componente: "Motherboard ASUS 1500", precio: 120 },
+      { componente: "Monitor ASC 543", precio: 250 },
+      { componente: "Motherboard ASUS 1200", precio: 100 },
+      { componente: "Motherboard MZI", precio: 30 },
+      { componente: "HDD Toyiva", precio: 90 },
+      { componente: "HDD Wezter Dishital", precio: 75 },
+      { componente: "RAM Quinston", precio: 110 },
+      { componente: "RAM Quinston Fury", precio: 230 }
+    ]
+  };
+  
